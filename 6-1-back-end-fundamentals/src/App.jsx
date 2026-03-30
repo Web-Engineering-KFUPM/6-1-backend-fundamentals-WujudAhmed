7946112c14685001d0a4dbf3b3a97f87fe504430
WujudAhmed <s202279120@kfupm.edu.sa>
2026-03-30 11:50:57 +0300

    To do 8&9 are done

--- 6-1-back-end-fundamentals/src/App.jsx
+++ 6-1-back-end-fundamentals/src/App.jsx
@@ -360,6 +360,8 @@ export default function App() {
   // Create state to store student data
   // Syntax hint:
   // const [student, setStudent] = useState(____);
+  const [student, setStudent] = useState(null);
+
 
   // TODO 8:
   // Request student data from the server when the page loads
@@ -369,6 +371,15 @@ export default function App() {
   //   .then((data) => {
   //     setStudent(data);
   //   });
+  useEffect(() => {
+   fetch('http://localhost:3000/student')
+     .then((res) => res.json())
+     .then((data) => {
+       setStudent(data);
+     });
+ }, []);
+
+  export default function App() {
 
   return (
     <main className="app-shell">
