157ba790406c19660d6c9c4bb63224ba72b196f7
WujudAhmed <s202279120@kfupm.edu.sa>
2026-03-30 11:41:48 +0300

    To do 1&2 are done

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,25 +1,8 @@
 // TODO 1: Import Express
-
+import express from 'express';
 
 
 // TODO 2: Create the Express app and store it in a variable named app
+const app = express();
 
-
-
-// TODO 3: Allow React to access the server
-
-
-
-// TODO 5: Create the home route "/"
-
-
-
-// TODO 6: Create the "/about" route
-
-
-
-// TODO 7: Create the "/student" route
-
-
-
-// TODO 4: Start the server on port 3000
+app.use(cors());
